90d7e5a4bef4f841587fd266ff5c465539655900
tyarkoni <tyarkoni@gmail.com>
2014-05-11 11:57:28 -0500

    added package files

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+import sys
+from distutils.core import setup
+
+# For some commands, use setuptools
+if len(set(('test', 'easy_install')).intersection(sys.argv)) > 0:
+    import setuptools
+
+extra_setuptools_args = {}
+if 'setuptools' in sys.modules:
+    extra_setuptools_args = dict(
+        tests_require=['nose'],
+        test_suite='nose.collector',
+        extras_require=dict(
+            test='nose>=0.10.1')
+    )
+
+# fetch version from within module
+with open(os.path.join('scythe', 'version.py')) as f:
+    exec(f.read())
+
+setup(name="scythe",
+      version=__version__,
+      description="Genetic algorithm-based measure abbreviation in Python.",
+      maintainer='Tal Yarkoni',
+      maintainer_email='tyarkoni@gmail.com',
+      url='http://github.com/tyarkoni/scythe',
+      packages=["scythe"],
+      package_data={'scythe': ['data/*'],
+                    'scythe.tests': ['data/*']
+                    },
+      **extra_setuptools_args
+      )
